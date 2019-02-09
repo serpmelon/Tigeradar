f6ef2437b37ea3da09063fb207de0ea2ff220b32
serpmelon <tyn243222791@126.com>
2019-02-09 22:36:28 +0800

    增加了配置文件异常类；增加了配置文件读取int Boolean类型方法；

--- src/main/java/com/togo/tigeradar/conf/PoolTigeradarConfig.java
+++ src/main/java/com/togo/tigeradar/conf/PoolTigeradarConfig.java
@@ -1,9 +1,23 @@
 package com.togo.tigeradar.conf;
 
-import org.springframework.beans.factory.annotation.Value;
-
 import com.togo.tigeradar.util.PropertiesReader;
 
+/**
+ * 
+ * <p>
+ * Class : com.togo.tigeradar.conf.PoolTigeradarConfig
+ * <p>
+ * Descdription:连接池配置类；对象初始化的时候会初始化配置文件中的设置；
+ *
+ * @author taiyn
+ * @version 1.0.0
+ *          <p>
+ *          --------------------------------------------------------------<br>
+ *          修改履历：<br>
+ *          <li>2019年2月9日，taiyn，创建文件；<br>
+ *          --------------------------------------------------------------<br>
+ *          </p>
+ */
 public enum PoolTigeradarConfig {
 
 	INSTANCE;
@@ -14,11 +28,56 @@ public enum PoolTigeradarConfig {
 	private boolean expectContinueEnabled;
 	private int maxTotal;
 	private int defaultMaxPreRoute;
-	
+
 	private final static String POOL_CONFIG_FILE = "poolTigeradarConf.properties";
+
+	private final static String POOL_CONNECT_TIMEOUT = "httpclient.pool.connect-timeout";
+	private final static String POOL_CONNECT_REQUEST_TIMEOUT = "httpclient.pool.connection-request-timeout";
+	private final static String POOL_SOCKET_TIMEOUT = "httpclient.pool.socket-timeout";
+	private final static String POOL_EXPECT_CONTINUE_ENABLED = "httpclient.pool.expect-continue-enabled";
+	private final static String POOL_MAX_TOTAL = "httpclient.pool.max-total";
+	private final static String POOL_DEFAULT_MAX_PRE_ROUTE = "httpclient.pool.default-max-pre-route";
+
+	private PropertiesReader reader;
 
 	private PoolTigeradarConfig() {
 
-		PropertiesReader reader = new PropertiesReader(POOL_CONFIG_FILE);
+		reader = new PropertiesReader(POOL_CONFIG_FILE);
+
+		connectTimeout = reader.readInt(POOL_CONNECT_TIMEOUT);
+		connectionRequestTimeout = reader.readInt(POOL_CONNECT_REQUEST_TIMEOUT);
+		socketTimeout = reader.readInt(POOL_SOCKET_TIMEOUT);
+		expectContinueEnabled = reader.readBoolean(POOL_EXPECT_CONTINUE_ENABLED);
+		maxTotal = reader.readInt(POOL_MAX_TOTAL);
+		defaultMaxPreRoute = reader.readInt(POOL_DEFAULT_MAX_PRE_ROUTE);
 	}
+
+	public int getConnectTimeout() {
+		return connectTimeout;
+	}
+
+	public int getConnectionRequestTimeout() {
+		return connectionRequestTimeout;
+	}
+
+	public int getSocketTimeout() {
+		return socketTimeout;
+	}
+
+	public boolean isExpectContinueEnabled() {
+		return expectContinueEnabled;
+	}
+
+	public int getMaxTotal() {
+		return maxTotal;
+	}
+
+	public int getDefaultMaxPreRoute() {
+		return defaultMaxPreRoute;
+	}
+
+	public static String getPoolConfigFile() {
+		return POOL_CONFIG_FILE;
+	}
+
 }
--- src/main/java/com/togo/tigeradar/util/PropertiesReader.java
+++ src/main/java/com/togo/tigeradar/util/PropertiesReader.java
@@ -4,13 +4,16 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.util.Properties;
 
+import org.springframework.util.StringUtils;
+
+import com.togo.tigeradar.exception.PropertyException;
+
 public class PropertiesReader {
 
 	private Properties pp = new Properties();
-	
+
 	public PropertiesReader(String fileName) {
-		InputStream in = PropertiesReader.class.getClassLoader()
-				.getResourceAsStream(fileName);
+		InputStream in = PropertiesReader.class.getClassLoader().getResourceAsStream(fileName);
 		try {
 			pp.load(in);
 		} catch (IOException e) {
@@ -18,9 +21,58 @@ public class PropertiesReader {
 			e.printStackTrace();
 		}
 	}
-	
-	public String readString(String key) throws IOException {
+
+	public String readString(String key) {
 
 		return pp.getProperty(key);
+	}
+
+	public int readInt(String key) {
+
+		String value = pp.getProperty(key);
+
+		if (StringUtils.isEmpty(value)) {
+
+			throw new PropertyException("配置文件中没有key对应的value, key:" + key);
+		}
+		int valInt;
+		try {
+
+			valInt = Integer.parseInt(value);
+		} catch (NumberFormatException e) {
+
+			throw new PropertyException("配置文件转换错误，String -> int, value: " + value);
+		}
+
+		return valInt;
+	}
+
+	/**
+	 * 
+	 * <p>
+	 * Method ：readBoolean
+	 * <p>
+	 * Description :返回boolean类型，如果值不为true，则为false
+	 *
+	 * @param key
+	 * @return
+	 * @author taiyn
+	 *         <p>
+	 *         --------------------------------------------------------------<br>
+	 *         修改履历：<br>
+	 *         <li>2019年2月9日，taiyn，创建方法；<br>
+	 *         --------------------------------------------------------------<br>
+	 *         </p>
+	 */
+	public boolean readBoolean(String key) {
+
+		String value = pp.getProperty(key);
+
+		if (StringUtils.isEmpty(value)) {
+
+			throw new PropertyException("配置文件中没有key对应的value, key:" + key);
+		}
+
+		return value.equalsIgnoreCase("true");
 	}
 }
