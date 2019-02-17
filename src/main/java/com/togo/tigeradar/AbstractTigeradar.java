a73f4dd14f0e1e5fa37c400a4031ac63fb090000
serpmelon <tyn243222791@126.com>
2019-02-18 07:46:51 +0800

    增加了基础的请求配置

--- src/main/java/com/togo/tigeradar/AbstractTigeradar.java
+++ src/main/java/com/togo/tigeradar/AbstractTigeradar.java
@@ -11,6 +11,7 @@ import java.util.Map;
 import org.apache.http.HttpEntity;
 import org.apache.http.HttpResponse;
 import org.apache.http.NameValuePair;
+import org.apache.http.client.config.RequestConfig;
 import org.apache.http.client.entity.UrlEncodedFormEntity;
 import org.apache.http.client.methods.CloseableHttpResponse;
 import org.apache.http.client.methods.HttpGet;
@@ -41,7 +42,13 @@ public abstract class AbstractTigeradar implements Tigeradar {
 
 	private Logger logger = LoggerFactory.getLogger(AbstractTigeradar.class);
 
+	private static RequestConfig requestConfig;
+
 	abstract CloseableHttpClient getHttpClient();
+
+	static {
+		initializeRequestConfig();
+	}
 
 	@Override
 	public HttpResponse get(String url) {
@@ -94,7 +101,7 @@ public abstract class AbstractTigeradar implements Tigeradar {
 		try {
 			httpPost.setEntity(new UrlEncodedFormEntity(params, "UTF-8"));
 		} catch (Exception e) {
-			e.printStackTrace();
+			logger.error("", e);
 		}
 
 		return doPost(httpPost);
@@ -120,6 +127,7 @@ public abstract class AbstractTigeradar implements Tigeradar {
 		CloseableHttpClient client = getHttpClient();
 
 		try {
+			httpGet.setConfig(requestConfig);
 			response = client.execute(httpGet);
 		} catch (IOException e) {
 			logger.error("", e);
@@ -149,7 +157,7 @@ public abstract class AbstractTigeradar implements Tigeradar {
 		HttpEntity entity = null;
 		try {
 
-			// post.setConfig(requestConfig);
+			post.setConfig(requestConfig);
 			// 执行请求
 			response = client.execute(post);
 			entity = response.getEntity();
@@ -174,6 +182,28 @@ public abstract class AbstractTigeradar implements Tigeradar {
 		return response;
 	}
 
-	// TODO 构建请求头
+	/**
+	 * 
+	 * <p>
+	 * Method ：initializeRequestConfig
+	 * <p>
+	 * Description : 初始化RequestConfig
+	 * 
+	 * @author taiyn
+	 *         <p>
+	 *         --------------------------------------------------------------<br>
+	 *         修改履历：<br>
+	 *         <li>2019年2月17日，taiyn，创建方法；<br>
+	 *         --------------------------------------------------------------<br>
+	 *         </p>
+	 */
+	private static void initializeRequestConfig() {
+
+		requestConfig = RequestConfig.custom() // RequestConfigBuilder
+				.setConnectionRequestTimeout(60000) //
+				.setConnectTimeout(60000) //
+				.setSocketTimeout(10000) //
+				.build();
+	}
 
 }
